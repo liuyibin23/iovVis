034d52366abf85b8e5bccd0320b9c2b787f306b3
lvyv <26896225@qq.com>
2018-12-11 21:39:04 +0800

    subscribe websocket event according to subscription.js

--- ui/src/app/integrated/integrated.controller.js
+++ ui/src/app/integrated/integrated.controller.js
@@ -22,8 +22,12 @@ import "cesium/Widgets/widgets.css";
 // import "cesium/Widgets/lighter.css";
 
 /*@ngInject*/
-export default function IntegratedController($scope, $filter, $mdMedia, $q, menu, $state) {
-
+export default function IntegratedController(
+    $scope, $filter, $mdMedia, $q, menu, $state, $timeout, 
+    $log, tbRaf, types, utils, timeService,
+    datasourceService, alarmService,  dashboardService, deviceService
+    ) {
+       
     var vm = this;
 
     vm.sectionColspan = sectionColspan;
@@ -31,31 +35,32 @@ export default function IntegratedController($scope, $filter, $mdMedia, $q, menu
     // $rootScope.forceFullscreen = !$rootScope.forceFullscreen;
     // $scope.searchConfig.searchEnabled = true;
     vm.isLibraryOpen = true;
-    
+
     var viewer = new Cesium.Viewer('cesiumContainer');
-	// var tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
-	// 	url : '../tilesets/TilesetWithDiscreteLOD/tileset.json'
+    // var tileset = viewer.scene.primitives.add(new Cesium.Cesium3DTileset({
+    // 	url : '../tilesets/TilesetWithDiscreteLOD/tileset.json'
     // }));
-    
-    $scope.$watch(function() { return $mdMedia('lg'); }, function() {
+
+    $scope.$watch(function () { return $mdMedia('lg'); }, function () {
         updateColumnCount();
     });
 
-    $scope.$watch(function() { return $mdMedia('gt-lg'); }, function() {
+    $scope.$watch(function () { return $mdMedia('gt-lg'); }, function () {
         updateColumnCount();
     });
 
     updateColumnCount();
 
-    vm.model = menu.getHomeSections();    
+    vm.model = menu.getHomeSections();
 
-    var integratedCtx = { 
+    /*订阅开始*/
+    var integratedCtx = {
         subscriptions: {},
         subscriptionApi: {
-            createSubscription: function(options, subscribe) {
+            createSubscription: function (options, subscribe) {
                 return createSubscription(options, subscribe);
             },
-            removeSubscription: function(id) {
+            removeSubscription: function (id) {
                 var subscription = integratedCtx.subscriptions[id];
                 if (subscription) {
                     subscription.destroy();
@@ -64,24 +69,43 @@ export default function IntegratedController($scope, $filter, $mdMedia, $q, menu
             }
         }
     };
+    var deferred = $q.defer();
 
     var subscriptionContext = {
         $scope: $scope,
         $q: $q,
         $filter: $filter,
-        // $timeout: $timeout,
-        // tbRaf: tbRaf,
-        // timeService: timeService,
-        // deviceService: deviceService,
-        // datasourceService: datasourceService,
-        // alarmService: alarmService,
-        // utils: utils,
+        $timeout: $timeout,
+        tbRaf: tbRaf,
+        timeService: timeService,
+        deviceService: deviceService,
+        datasourceService: datasourceService,
+        alarmService: alarmService,
+        utils: utils,
         // widgetUtils: widgetContext.utils,
         // dashboardTimewindowApi: dashboardTimewindowApi,
-        // types: types,
-        // getStDiff: dashboardService.getServerTimeDiff,
+        types: types,
+        getStDiff: dashboardService.getServerTimeDiff,
         // aliasController: aliasController
     };
+    var subscriptionOptions = {
+        datasources: [],
+        callbacks: {
+            onDataUpdated: onDataUpdated,
+            onDataUpdateError: onDataUpdateError
+        }
+    };
+
+    createSubscription(subscriptionOptions, true).then(
+        function success(subscription) {
+            integratedCtx.defaultSubscription = subscription;
+            $log.info('AWEN-->subscription is created in integratedCtx.defaultSubscription');
+            deferred.resolve();
+        },
+        function fail() {
+            deferred.reject();
+        }
+    );
 
     function createSubscription(options, subscribe) {
         var deferred = $q.defer();
@@ -101,10 +125,47 @@ export default function IntegratedController($scope, $filter, $mdMedia, $q, menu
         return deferred.promise;
     }
 
+    function onDataUpdated(subscription, apply) {
+        // var value = false;
+        var data = subscription.data;
+        if (data.length) {
+            $log.log('AWEN-->DATA');
+            // var keyData = data[0];
+            // if (keyData && keyData.data && keyData.data[0]) {
+            //     var attrValue = keyData.data[0][1];
+            //     if (attrValue) {
+            //         var parsed = null;
+            //         try {
+            //             parsed = vm.parseValueFunction(angular.fromJson(attrValue));
+            //         } catch (e){/**/}
+            //         value = parsed ? true : false;
+            //     }
+            // }
+        }
+        /*
+        setValue(value);
+        if (apply) {
+            $scope.$digest();
+        }
+        */
+    }
+
+    function onDataUpdateError(subscription, e) {
+        $log.error('AWEN-->ERR');
+        // var exceptionData = utils.parseException(e);
+        // var errorText = exceptionData.name;
+        // if (exceptionData.message) {
+        //     errorText += ': ' + exceptionData.message;
+        // }
+        // onError(errorText);
+    }
+
+    /*订阅结束*/
+
     // dashboard id---> d96c1d40-de3e-11e8-90b2-6708b10d2f5b 战略情报
     // dashboard id---> af5dadb0-e5ba-11e8-be95-f3713e6700c3 桥梁数据展示
     function openDashboard(dashboardID) {
-        $state.go('home.integrated.dashboard', {dashboardId: dashboardID});
+        $state.go('home.integrated.dashboard', { dashboardId: dashboardID });
     }
 
     function updateColumnCount() {
