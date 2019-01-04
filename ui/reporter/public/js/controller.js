df9d005626f0bf6077f44ada2c0cf77cbb97397f
lvyv <26896225@qq.com>
2019-01-05 01:15:43 +0800

    add reporter generator web app

--- ui/reporter/public/js/controller.js
+++ ui/reporter/public/js/controller.js
@@ -1,16 +1,39 @@
 /* eslint-disable no-console */
 
+var progressbar = $("#progressbar");
+max = progressbar.attr('max');
+value = progressbar.val();
+time = (1000 / max) * 5;
+
+var loading = function () {
+  value += 1;
+  addValue = progressbar.val(value);
+  $(".progress_value").html(value + "%");
+  if (value == max)
+    value = 0;
+};
+
+var animate;
+
+var bar = $(".html5-progressbar");
 console.log('Starting swapi demo');
 
 const createReport = docxTemplates; // eslint-disable-line
 
 // callback when a template has been selected
 async function onTemplateChosen(event) {
+  value = 0;
+  bar.show();
+  animate = setInterval(function () {
+    loading();
+  }, time);
+
+  let flag = false;
   console.log('Template chosen');
   // read the file in an ArrayBuffer
   const content = await readFile(this.files[0]);
   // select the right data source, depending on selected input
-  let data = null;
+  // let data = null;
   // if (event.target.id === 'inputSwapi') {
   //   data = query => postQuery('/swapi', query); // query to swapi webservice
   // } else if (event.target.id === 'inputQuill') {
@@ -18,34 +41,44 @@ async function onTemplateChosen(event) {
   // }
   // fill the template
   console.log('Creating report (can take some time) ...');
-  const doc = await createReport({ template: content, data:data, 
+  const doc = await createReport({
+    template: content, data: { name: 'Awen', createTime: new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') },
     additionalJsContext: {
       genIMG: async (type, tsw, w_cm, h_cm) => {
-        console.log('--- try to axios ---',type,tsw);
+        console.log('--- try to axios ---', type, tsw);
         let data = [];
         await axios.post('/swapi', {
           query: ' {allFilms { films { title, releaseDate } } }',
           chartType: type,
           chartTsw: tsw
         })
-        .then(function (response) {
-          console.log(response);
-          const dataUrl = response.data;
-          data = dataUrl.slice('data:image/png;base64,'.length);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+          .then(function (response) {
+            console.log(response);
+            const dataUrl = response.data;
+            data = dataUrl.slice('data:image/png;base64,'.length);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+        flag = true;
         return { width: w_cm, height: h_cm, data, extension: '.png' };
       }
     }
+  }).catch(err => {
+    console.log(err);
+    bar.show();
+    clearInterval(animate);
   });
   // generate output file for download
-  downloadBlob(
-    doc,
-    'report.docx',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-  );
+  if (flag) {
+    downloadBlob(
+      doc,
+      'report.docx',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+    );
+    bar.hide();
+    clearInterval(animate);
+  }
 }
 
 // ==============================================
@@ -56,8 +89,6 @@ async function onTemplateChosen(event) {
 // (need to wait for DOM to be loaded otherwise input will be undefined)
 document.addEventListener('DOMContentLoaded', () => {
   let inputElement = document.getElementById('inputSwapi');
-  inputElement.addEventListener('change', onTemplateChosen, false);
-  inputElement = document.getElementById('inputQuill');
   inputElement.addEventListener('change', onTemplateChosen, false);
 });
 
