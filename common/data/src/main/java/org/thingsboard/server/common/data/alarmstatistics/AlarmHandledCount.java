5e4dd8a5423365a64a7c27f59aa06653df5d2c93
Mornin.Z <ztao7516@gmail.com>
2019-01-08 18:17:35 +0800

    1. 修改dashboard报警接口 2. 添加了观测点报警接口

---
+++ common/data/src/main/java/org/thingsboard/server/common/data/alarmstatistics/AlarmHandledCount.java
@@ -0,0 +1,67 @@
+package org.thingsboard.server.common.data.alarmstatistics;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by ztao at 2019/1/4 16:19.
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlarmHandledCount {
+    private int unackedOverdue;
+    private int ackedOverdue;
+    private int clearedOverdue;
+
+    private int unackedWithinDue;
+    private int ackedWithinDue;
+    private int clearedWithinDue;
+
+    private int totalAlarmCount;
+    private int createdOfToday;
+
+    public int unackedOverduePlus(int count) {
+        unackedOverdue += count;
+        return unackedOverdue;
+    }
+
+    public int ackedOverduePlus(int count) {
+        ackedOverdue += count;
+        return ackedOverdue;
+    }
+
+    public int clearedOverduePlus(int count) {
+        clearedOverdue += count;
+        return clearedOverdue;
+    }
+
+    public int unackedWithinDuePlus(int count) {
+        unackedWithinDue += count;
+        return unackedWithinDue;
+    }
+
+    public int ackedWithinDuePlus(int count) {
+        ackedWithinDue += count;
+        return ackedWithinDue;
+    }
+
+    public int clearedWithinDuePlus(int count) {
+        clearedWithinDue += count;
+        return clearedWithinDue;
+    }
+
+    public int totalAlarmCountPlus(int count) {
+        totalAlarmCount += count;
+        return totalAlarmCount;
+    }
+
+    public int createdOfTodayPlus(int count) {
+        createdOfToday += count;
+        return createdOfToday;
+    }
+
+}
