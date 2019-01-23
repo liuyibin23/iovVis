4fc9c08db73849d6327e3f565d797e7e25019c88
PoppyRobot <azx12358@126.com>
2019-01-23 18:03:54 +0800

    上传template文件

--- apiGateway/routers/templates-router.js
+++ apiGateway/routers/templates-router.js
@@ -1,21 +1,21 @@
 var express = require('express')
 var router = express.Router()
-var toks  = require('../middleware/token-verifier')
-var util  = require('./utils')
+var toks = require('../middleware/token-verifier')
+var util = require('./utils')
 const fs = require('fs')
 const axios = require('axios')
+var request = require('request')
 
-var multipart = require('connect-multiparty');  
+var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
 
 // Get asset
-async function getAsset(assetId, token)
-{
+async function getAsset(assetId, token) {
   let get_asset_api = util.getAPI() + 'assets?assetIds=' + assetId;
   let assetInfo = await util.getSync(get_asset_api, {
-      headers: {
-          "X-Authorization": "Bearer " + token
-      }
+    headers: {
+      "X-Authorization": "Bearer " + token
+    }
   });
 
   if (!assetInfo) return;
@@ -23,20 +23,20 @@ async function getAsset(assetId, token)
   let len = assetInfo.length;
   let data = new Array();
   if (len > 0) {
-      console.log('1. Get asset info:%d', len);
-      assetInfo.forEach(info => {
-          let _dt = {id:'', name:'', type:''};
-          _dt.id   = info.id.id;
-          _dt.name = info.name;
-          _dt.type = info.type;
+    console.log('1. Get asset info:%d', len);
+    assetInfo.forEach(info => {
+      let _dt = { id: '', name: '', type: '' };
+      _dt.id = info.id.id;
+      _dt.name = info.name;
+      _dt.type = info.type;
 
-          data.push(_dt);
-      });
+      data.push(_dt);
+    });
 
-      return data;
+    return data;
   }
 
-  let _dt = {id:'', name:'', type:''};
+  let _dt = { id: '', name: '', type: '' };
   data.push(_dt);
   return data;
 }
@@ -45,34 +45,33 @@ async function getAsset(assetId, token)
 async function getAttributes(assetId, token) {
   let get_attributes_api = util.getAPI() + `/plugins/telemetry/ASSET/${assetId}/values/attributes/SERVER_SCOPE`;
   let attributesInfo = await util.getSync(get_attributes_api, {
-      headers: {
-          "X-Authorization": "Bearer " + token
-      }
+    headers: {
+      "X-Authorization": "Bearer " + token
+    }
   });
 
   if (!attributesInfo) return;
   let data = new Array();
   let len = attributesInfo.length;
   if (len > 0) {
-      attributesInfo.forEach(info => {
-        if (info.key === 'TEMPLATES')
-        {
-          data.push(info);
-          return data;
-        }
-      });
+    attributesInfo.forEach(info => {
+      if (info.key === 'TEMPLATES') {
+        data.push(info);
+        return data;
+      }
+    });
   }
 }
 
 // middleware that is specific to this router
-router.use(function timeLog (req, res, next) {
+router.use(function timeLog(req, res, next) {
   console.log('Templat Time: ', Date.now())
   next()
 })
 
 // define the home page route
 router.get('/', function (req, res) {
-  res.status(200).json({result: 'Templates Api home page'});
+  res.status(200).json({ result: 'Templates Api home page' });
 })
 
 // define the about route
@@ -90,23 +89,22 @@ router.get('/:assetId', async function (req, res) {
 
   let get_attributes_api = util.getAPI() + `/plugins/telemetry/ASSET/${assetID}/values/attributes/SERVER_SCOPE`;
 
-  axios.get(get_attributes_api,  {
+  axios.get(get_attributes_api, {
     headers: {
-        "X-Authorization": "Bearer " + token
+      "X-Authorization": "Bearer " + token
     }
   })
-  .then((resp)=>{
-    resp.data.forEach(info => {
-      if (info.key === 'TEMPLATES')
-      {
-        info.value = JSON.parse(info.value);
-        res.status(200).json(info);
-      }
+    .then((resp) => {
+      resp.data.forEach(info => {
+        if (info.key === 'TEMPLATES') {
+          info.value = JSON.parse(info.value);
+          res.status(200).json(info);
+        }
+      });
+    })
+    .catch((err) => {
+      res.status(500).json('');
     });
-  })
-  .catch((err)=>{
-    res.status(500).json('');
-  });
 })
 
 // function to encode file data to base64 encoded string
@@ -132,73 +130,85 @@ function base64_decode(base64str, file) {
 async function addDocx2asset(assetId, docx, tk) {
   let docxRes;
   try {
-      let sBase64 = 'data:doc/docx;base64,' + base64_encode(docx);
-      let file64 = 'file=' + encodeURIComponent(sBase64);
-      let host = 'http://sm.schdri.com';
-      docxRes = await axios.post(host + '/api/file/upload/base64',
-          file64,
-          {
-              headers: {
-                  "Content-Type": 'application/x-www-form-urlencoded'
-              }
-          });
-      let fileUrl = host + '/' + docxRes.data.fileId;
-      let apiUrl = `${host}/api/plugins/telemetry/ASSET/${assetId}/attributes/SERVER_SCOPE`;
-      let assetRes = await axios.post(apiUrl, { rpt_docx: fileUrl },
-          {
-              headers: {
-                  "X-Authorization": "Bearer " + tk
-              }
-          });
+    let sBase64 = 'data:doc/docx;base64,' + base64_encode(docx);
+    let file64 = 'file=' + encodeURIComponent(sBase64);
+    let host = 'http://sm.schdri.com';
+    docxRes = await axios.post(host + '/api/file/upload/base64',
+      file64,
+      {
+        headers: {
+          "Content-Type": 'application/x-www-form-urlencoded'
+        }
+      });
+    let fileUrl = host + '/' + docxRes.data.fileId;
+    let apiUrl = `${host}/api/plugins/telemetry/ASSET/${assetId}/attributes/SERVER_SCOPE`;
+    let assetRes = await axios.post(apiUrl, { rpt_docx: fileUrl },
+      {
+        headers: {
+          "X-Authorization": "Bearer " + tk
+        }
+      });
 
-      return assetRes;
+    return assetRes;
   } catch (err) {
-      throw new Error("Add docx to asset failed!");
+    throw new Error("Add docx to asset failed!");
   }
 }
 
+router.post('/abc', multipartMiddleware, async function (req, res) {
+  let msg = 'Post ID:' + req.params.id + 'template_name:' + req.body.template_name;
+
+})
+
 //POST
-router.post('/:id', multipartMiddleware, async function(req, res){
+router.post('/:id', multipartMiddleware, async function (req, res) {
   let msg = 'Post ID:' + req.params.id + 'template_name:' + req.body.template_name;
 
   let fileInfo = req.files.template_file;
 
-  let info = {'filename':fileInfo.originalFilename,
-    'path':fileInfo.path,
-    'size':fileInfo.size,
-    'type':fileInfo.type
+  let info = {
+    'filename': fileInfo.originalFilename,
+    'path': fileInfo.path,
+    'size': fileInfo.size,
+    'type': fileInfo.type
   }
 
   let token = toks.getTokenStr();
-    // 上传到服务器
-  // var promise = addDocx2asset(req.params.id, fileInfo.path, token);
-  // promise.then(function(err, data){
-  //   if (err) 
-  //     res.status(200).json('error');
-  //    // return console.error(err);
-  
-  //    res.status(200).json('Finish');
-  // })
-  
-  // 保存到属性表
-  // http://cf.beidouapp.com:8080/api/plugins/telemetry/ASSET/265c7510-1df4-11e9-b372-db8be707c5f4/SERVER_SCOPE
-  let url = util.getAPI() + 'plugins/telemetry/ASSET/' + req.params.id + '/SERVER_SCOPE';
+  //上传到服务器
+  //addDocx2asset(req.params.id, fileInfo.path, token);
 
-  let str = [{"template_name":fileInfo.originalFilename,
-      "template_url:": 'http://127.0.0.1:80/' + fileInfo.originalFilename
+  var formData = {
+    file: fs.createReadStream(fileInfo.path),
+  };
+  let host = 'http://sm.schdri.com:80/'
+  let uploadFileHost = host + 'api/file/upload/'
+  request.post({ url:uploadFileHost, formData: formData }, function (err, httpResponse, body) {
+    if (err) {
+      return console.error('upload failed:', err);
+    }
+    console.log('Upload successful!  Server responded with:', body);
+
+    // 保存到属性表
+    // http://cf.beidouapp.com:8080/api/plugins/telemetry/ASSET/265c7510-1df4-11e9-b372-db8be707c5f4/SERVER_SCOPE
+    let url = util.getAPI() + 'plugins/telemetry/ASSET/' + req.params.id + '/SERVER_SCOPE';
+    let bodyData = JSON.parse(body)
+    let str = [{
+      "template_name": req.body.template_name,
+      "template_url:": host + bodyData.fileId
     }];
-  let val = JSON.stringify(str);
+    let val = JSON.stringify(str);
 
-  let data = {
-    "TEMPLATES":`${val}`
-  }
-  axios.post(url, (data), { headers: { "X-Authorization": "Bearer " + toks.getTokenStr() } })
-  .then(response =>{
-    res.status(response.status).json('成功创建报表模板并关联到资产。');
-  })
-  .catch(err => {
-    console.log(err);
-    res.end();
+    let data = {
+      "TEMPLATES": `${val}`
+    }
+    axios.post(url, (data), { headers: { "X-Authorization": "Bearer " + toks.getTokenStr() } })
+      .then(response => {
+        res.status(response.status).json('成功创建报表模板并关联到资产。');
+      })
+      .catch(err => {
+        console.log(err);
+        res.end();
+      });
   });
 
   // 返回url地址
@@ -206,7 +216,7 @@ router.post('/:id', multipartMiddleware, async function(req, res){
 
 
 //DELETE
-router.delete('/:id', async function(req, res){
+router.delete('/:id', async function (req, res) {
   let msg = 'Delete ID:' + req.params.id + ' Name:' + req.query.templateName;
 
   res.status(200).json(msg);
