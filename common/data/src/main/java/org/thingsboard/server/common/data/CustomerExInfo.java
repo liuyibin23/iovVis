04eced65dfc7483460b2c5fb1a495d400c231899
liuyibin <liuyibin23@163.com>
2019-01-25 12:30:44 +0800

    /api/admin/customers查询customer时返回adminUser姓名列表

---
+++ common/data/src/main/java/org/thingsboard/server/common/data/CustomerExInfo.java
@@ -0,0 +1,33 @@
+package org.thingsboard.server.common.data;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class CustomerExInfo extends Customer {
+
+    private List<String> adminUserNameList = new ArrayList<>();
+
+    public CustomerExInfo(){}
+    public CustomerExInfo(Customer customer){
+        this.id = customer.getId();
+        this.setAdditionalInfo(customer.getAdditionalInfo());
+        this.address = customer.address;
+        this.address2 = customer.address2;
+        this.city = customer.city;
+        this.country = customer.country;
+        this.email = customer.email;
+        this.phone = customer.phone;
+        //search_text
+        this.state = customer.state;
+        this.setTenantId(customer.getTenantId());
+        this.setTitle(customer.getTitle());
+        this.zip = customer.zip;
+        this.setAdminCount(customer.getAdminCount());
+        this.setUserCount(customer.getUserCount());
+        this.setInfrastructureCount(customer.getInfrastructureCount());
+    }
+
+}
