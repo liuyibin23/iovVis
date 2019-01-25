d9d2cee52784f41820814dc8a2356324ae5b10cd
PoppyRobot <azx12358@126.com>
2019-01-25 17:28:57 +0800

    返回图表base64数据接口

---
+++ apiGateway/routers/echarts-Router.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+var chart_area = require('./echarts/area');
+var chart_pie  = require('./echarts/pie');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+const node_echarts = require('node-echarts');
+
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+    console.log('echarts Time: ', Date.now())
+    next()
+})
+
+// define the home page route
+router.get('/', function (req, res) {
+    res.send('Echarts Api home page')
+})
+
+// define the about route
+router.get('/about', function (req, res) {
+    res.send('About Echarts')
+})
+
+router.post('/:id', async function (req, res) {
+    let assetID = req.params.id;
+})
+ 
+
+function processData(option, params, res){
+    if (!option)
+    {
+        let resMsg = {
+            "code": '404',
+            "message:": '访问资源不存在。'
+        };
+        res.status(404).json(resMsg);  
+        return;  
+    }
+
+    let config = {
+        width: params.chartWidth   ? params.chartHeight * 100 : 500, // Image width, type is number.
+        height: params.chartHeight ? params.chartHeight * 100 : 400, // Image height, type is number.
+        option: option, // Echarts configuration, type is Object.
+        //If the path  is not set, return the Buffer of image.
+        // path:  '', // Path is filepath of the image which will be created.
+        enableAutoDispose: true  //Enable auto-dispose echarts after the image is created.
+    }
+    let bytes = node_echarts(config);
+    if (bytes) {
+        let resMsg = {
+            "code": '200',
+            "message:": 'data:image/png;base64,' + bytes.toString('base64')
+        };
+        res.status(200).json(resMsg);
+    }
+}
+
+router.get('/:id', async function (req, res) {
+    let type   = req.params.id;
+    let params = req.query;
+    console.log('type:', type, 'query:', req.query);
+    let token = req.headers['x-authorization'];;
+    switch (type) {
+        case 'area':
+            option = await chart_area.fillData(params, token, res, processData);
+            break;
+        case 'pie':
+        {
+            option = chart_pie;
+            option.title.text = '设备ID:' + params.devid;
+            break;
+        }
+        default:
+    }
+})
+
+module.exports = router
---
+++ apiGateway/routers/echarts/area.js
@@ -0,0 +1,74 @@
+const axios = require('axios');
+             
+
+let  option = {
+    title: {
+        text: ''
+    },
+    tooltip : {
+        trigger: 'axis'
+    },
+    legend: {
+        data:['']
+    },
+    toolbox: {
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis : [
+        {
+            type : 'category',
+            boundaryGap : false,
+            data : []
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value'
+        }
+    ],
+    series : [
+        {
+            name:'搜索引擎',
+            type:'line',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'top'
+                }
+            },
+            areaStyle: {normal: {}},
+            data:[]
+        }
+    ]
+};
+
+var chart_data = {
+	name: 'chart_data',
+	version: '1.0.0',
+
+    fillData: async function(params, token, res, callback) {
+        console.log(token);
+        let apiUrl = `http://cf.beidouapp.com:8080/api/plugins/telemetry/DEVICE/${params.devid}/values/timeseries?limit=100&agg=NONE&keys=crackWidth&startTs=${params.startTime}&endTs=${params.endTime}`;
+        await axios.get(apiUrl, { headers: { "X-Authorization": token } })
+        .then((resp) => {
+            let data = resp.data.crackWidth;
+            option.title.text = '设备ID: ' + params.devid;
+            data.forEach((element, index, array) => {
+                var val = Math.round(Number.parseFloat(element.value) * 100) / 100;
+                option.series[0].data.push(val);
+                option.xAxis[0].data.push(index);
+            });
+            callback(option, params, res);
+        })
+        .catch((err) =>{
+            callback(null, params, res);
+        });  
+    }
+}
+module.exports = chart_data;
