58c952df9bb7456c1e85e7a1fd341b7df92414b0
lvyv <26896225@qq.com>
2019-01-26 19:21:13 +0800

    add warnings get and post api.

--- apiGateway/routers/warnings-router.js
+++ apiGateway/routers/warnings-router.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 const util = require('./utils');
 const router = express.Router();
 
-// define the home page route
+// define warnings route
 router.get('/:assetId', async function (req, res) {
   let assetID = req.params.assetId;
   let token = req.headers['x-authorization'];
-  let get_attributes_api = util.getAPI() + `/plugins/telemetry/ASSET/${assetID}/values/attributes/SERVER_SCOPE`;
+  let get_attributes_api = util.getAPI() + `plugins/telemetry/ASSET/${assetID}/values/attributes/SERVER_SCOPE`;
 
   axios.get(get_attributes_api, {
     headers: {
@@ -26,6 +26,50 @@ router.get('/:assetId', async function (req, res) {
           resMsg.info = info;
         }
       });
+      res.status(resp.status).json(resMsg);
+    })
+    .catch((err) => {
+      let status = err.response.status
+      if (status == 401) {
+        let resMsg = {
+          "code": `${err.response.status}`,
+          "message:": '无授权访问。'
+        };
+        res.status(err.response.status).json(resMsg);
+      }
+      else if (status == 500) {
+        let resMsg = {
+          "code": `${err.response.status}`,
+          "message:": '服务器内部错误。'
+        };
+        res.status(err.response.status).json(resMsg);
+      }
+      else if (status == 404) {
+        let resMsg = {
+          "code": `${err.response.status}`,
+          "message:": '访问资源不存在。'
+        };
+        res.status(err.response.status).json(resMsg);
+      }
+    });
+})
+
+// define warnings route
+router.post('/:assetId', async function (req, res) {
+  let assetID = req.params.assetId;
+  let token = req.headers['x-authorization'];
+  let get_attributes_api = util.getAPI() + `plugins/telemetry/ASSET/${assetID}/attributes/SERVER_SCOPE`;
+
+  axios.post(get_attributes_api, req.query, {
+    headers: {
+      "X-Authorization": token
+    }
+  })
+    .then((resp) => {
+      let resMsg = {
+        "code": `${resp.status}`,
+        info: {}
+      };
       res.status(resp.status).json(resMsg);
     })
     .catch((err) => {
