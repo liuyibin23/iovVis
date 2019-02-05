4c06e1997794b369be6cddaa348866607216f997
lvyv <26896225@qq.com>
2019-02-06 02:51:44 +0800

    startup shell script.

--- apiGateway/startGw.sh
+++ apiGateway/startGw.sh
@@ -1,2 +1,5 @@
+cd /opt/lvyu
+svn co https://github.com/lvyv/iovVis/trunk/apiGateway
+cd apiGateway
 sudo kill -9 $(ps aux | grep node | awk '{print $2}')
 sudo nohup /usr/local/bin/node ./server > apiGw.log 2>apiGw.err &
