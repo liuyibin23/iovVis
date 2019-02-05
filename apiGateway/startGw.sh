cb4403e2704f8339a1322b9c780a09eb32d01565
lvyv <26896225@qq.com>
2019-02-06 03:07:58 +0800

    shell

--- apiGateway/startGw.sh
+++ apiGateway/startGw.sh
@@ -3,3 +3,4 @@ svn co https://github.com/lvyv/iovVis/trunk/apiGateway
 cd apiGateway
 sudo kill -9 $(ps aux | grep node | awk '{print $2}')
 sudo nohup /usr/local/bin/node ./server > apiGw.log 2>apiGw.err &
+echo === ok ===
