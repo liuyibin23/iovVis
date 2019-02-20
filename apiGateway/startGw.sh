72ffdfbda3c66849e9707bd3d9ecdfebb26de57d
lvyv <26896225@qq.com>
2019-02-20 18:09:41 +0800

    startGw modify.

--- apiGateway/startGw.sh
+++ apiGateway/startGw.sh
@@ -1,6 +1,7 @@
 cd /opt/lvyu
 svn co https://github.com/lvyv/iovVis/trunk/apiGateway
 cd apiGateway
+npm install
 sudo kill -9 $(ps aux | grep [n]ode | awk '{print $2}')
-sudo nohup /usr/local/bin/node ./server > apiGw.log 2>apiGw.err &
+sudo nohup node ./server > apiGw.log 2>apiGw.err &
 echo ===restart apigateway ok ===
