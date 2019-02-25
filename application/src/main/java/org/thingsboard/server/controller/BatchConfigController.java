f1c6445e2580e99a8f3be19b64ecd50e9551949f
shjmqqc <shjmqqc@189.cn>
2019-02-25 16:22:03 +0800

    修改批量接口

--- application/src/main/java/org/thingsboard/server/controller/BatchConfigController.java
+++ application/src/main/java/org/thingsboard/server/controller/BatchConfigController.java
@@ -26,7 +26,6 @@ import org.thingsboard.server.common.data.kv.BaseAttributeKvEntry;
 import org.thingsboard.server.common.data.kv.StringDataEntry;
 import org.thingsboard.server.common.data.relation.EntityRelation;
 import org.thingsboard.server.common.data.relation.RelationTypeGroup;
-import org.thingsboard.server.common.data.security.Authority;
 import org.thingsboard.server.common.data.security.DeviceCredentials;
 import org.thingsboard.server.common.msg.cluster.SendToClusterMsg;
 import org.thingsboard.server.service.security.AccessValidator;
@@ -47,79 +46,21 @@ public class BatchConfigController extends BaseController {
 	@PreAuthorize("hasAuthority('SYS_ADMIN')")
 	@RequestMapping(value = "/api/batchconfig/devices/{assetId}", method = RequestMethod.GET)
 	@ResponseBody
-	public List<DeviceAutoLogon> getDevices(@PathVariable("assetId") String assetId,@RequestParam String tenantIdStr) {
+	public List<DeviceAutoLogon> getDevices(@PathVariable("assetId") String assetId) {
 		try {
 
-			TenantId tenantIdTmp = new TenantId(toUUID(tenantIdStr));
-			checkTenantId(tenantIdTmp);
-			TenantId tenantId = tenantService.findTenantById(tenantIdTmp).getId();
-
 			AssetId aid = new AssetId(UUID.fromString(assetId));
-			Asset a = checkNotNull(assetService.findAssetById(tenantId, aid));
+			Asset a = checkNotNull(assetService.findAssetById(null,aid));
 
-			List<EntityRelation> relations = checkNotNull(relationService.findByFromAndType(tenantId, a.getId(), EntityRelation.CONTAINS_TYPE, RelationTypeGroup.COMMON));
+			List<EntityRelation> relations = checkNotNull(relationService.findByFromAndType(null, a.getId(), EntityRelation.CONTAINS_TYPE, RelationTypeGroup.COMMON));
 
-			return getDevicesAttrib(tenantId,relations);
+			return getDevicesAttrib(null, relations);
 		} catch (Exception e) {
 			e.printStackTrace();
 			handleException(e);
 		}
 
 		return null;
-	}
-
-	/**
-	 * @Description: 获取设备属性并生成列表
-	 * @Author: ShenJi
-	 * @Date: 2018/12/29
-	 * @Param:
-	 * @return:
-	 */
-	private List<DeviceAutoLogon> getDevicesAttrib(TenantId tenantId,List<EntityRelation> relations) {
-		List<DeviceAutoLogon> ret = new ArrayList<>();
-
-		relations.stream()
-				.filter(p -> {
-					return p.getTo().getEntityType() == EntityType.DEVICE;
-				})
-				.forEach((relation) -> {
-					DeviceAutoLogon deviceAutoLogon = new DeviceAutoLogon();
-					try {
-						List<AttributeKvEntry> attributeKvEntries = attributesService.findAll(tenantId,relation.getTo(),"CLIENT_SCOPE").get();
-						Map<String, Object> clientMap = new HashMap<>();
-						attributeKvEntries.forEach((attributeKvEntry -> {
-							clientMap.put(attributeKvEntry.getKey(),attributeKvEntry.getValue().toString());
-						}));
-						deviceAutoLogon.setDeviceClientAttrib(MAPPER.readValue(MAPPER.writeValueAsString(clientMap), DeviceClientAttrib.class));
-
-						attributeKvEntries = attributesService.findAll(tenantId,relation.getTo(),"SERVER_SCOPE").get();
-						Map<String, Object> serverMap = new HashMap<>();
-						attributeKvEntries.forEach((attributeKvEntry -> {
-							serverMap.put(attributeKvEntry.getKey(),attributeKvEntry.getValue().toString());
-						}));
-						deviceAutoLogon.setDeviceServerAttrib(MAPPER.readValue(MAPPER.writeValueAsString(serverMap), DeviceServerAttrib.class));
-
-						attributeKvEntries = attributesService.findAll(tenantId,relation.getTo(),"SHARED_SCOPE").get();
-						Map<String, Object> shareMap = new HashMap<>();
-						attributeKvEntries.forEach((attributeKvEntry -> {
-							shareMap.put(attributeKvEntry.getKey(),attributeKvEntry.getValue().toString());
-						}));
-						deviceAutoLogon.setDeviceShareAttrib(MAPPER.readValue(MAPPER.writeValueAsString(shareMap), DeviceShareAttrib.class));
-
-						deviceAutoLogon.setSystemDeviceId(relation.getTo().getId().toString());
-
-						ret.add(deviceAutoLogon);
-
-					}
-					catch (Exception e){
-						log.info("Get device client attrib error:"+relation.getTo().getId());
-						e.printStackTrace();
-						handleException(e);
-					}
-
-				});
-
-		return ret;
 	}
 
 	/**
@@ -134,30 +75,27 @@ public class BatchConfigController extends BaseController {
 	@ResponseStatus(value = HttpStatus.OK)
 	@ResponseBody
 	public DeferredResult<ResponseEntity> saveDevices(@PathVariable("assetId") String assetId,
-													  @RequestParam String tenantIdStr,
 													  @RequestBody List<DeviceAutoLogon> devicesSaveRequest) {
 		try {
-			TenantId tenantIdTmp = new TenantId(toUUID(tenantIdStr));
-			checkTenantId(tenantIdTmp);
-			TenantId tenantId = tenantService.findTenantById(tenantIdTmp).getId();
 
 			AssetId aid = new AssetId(UUID.fromString(assetId));
-			Asset a = assetService.findAssetById(tenantId, aid);
+			Asset a = assetService.findAssetById(null, aid);
 			if (a == null) {
-				return null;
+				throw new ThingsboardException(ThingsboardErrorCode.INVALID_ARGUMENTS);
 			}
 			devicesSaveRequest.forEach((deviceInfo) -> {
 
 				String uidStr = assetId + "|" + deviceInfo.getDeviceShareAttrib().getIp() + "|" + deviceInfo.getDeviceShareAttrib().getChannel();
 				deviceInfo.getDeviceServerAttrib().setName(uidStr.hashCode() + "");
-				Device device = deviceService.findDeviceByTenantIdAndName(tenantId, deviceInfo.getDeviceServerAttrib().getName());
+				Device device = deviceService.findDeviceByTenantIdAndName(a.getTenantId(), deviceInfo.getDeviceServerAttrib().getName());
 				//region 如果设备不存在，创建设备
 				if (device == null) {
 					device = new Device();
 					device.setName(deviceInfo.getDeviceServerAttrib().getName());
 					device.setType(deviceInfo.getDeviceShareAttrib().getSensorType());
 					try {
-						device.setTenantId(tenantId);
+						device.setTenantId(a.getTenantId());
+						device.setCustomerId(a.getCustomerId());
 						Device savedDevice = deviceService.saveDevice(device);
 						device = savedDevice;
 						actorService
@@ -189,42 +127,50 @@ public class BatchConfigController extends BaseController {
 				deviceInfo.setSystemDeviceId(device.getUuidId().toString());
 				ObjectMapper mapper = new ObjectMapper();
 				try {
-					DeviceCredentials deviceCredentials = checkNotNull(deviceCredentialsService.findDeviceCredentialsByDeviceId(tenantId, device.getId()));
+					DeviceCredentials deviceCredentials = checkNotNull(deviceCredentialsService.findDeviceCredentialsByDeviceId(a.getTenantId(), device.getId()));
 					deviceInfo.getDeviceShareAttrib().setToken(deviceCredentials.getCredentialsId());
-
-					String t = mapper.writeValueAsString(deviceInfo.getDeviceServerAttrib());
-					Map m = mapper.readValue(t, Map.class);
-					List<AttributeKvEntry> attributes = new ArrayList<>();
-					m.forEach((key, value) -> {
-						if (value != null) {
-							attributes.add(new BaseAttributeKvEntry(new StringDataEntry(key.toString(), value.toString()), System.currentTimeMillis()));
-						}
-					});
 					EntityId entityId = EntityIdFactory.getByTypeAndUuid(EntityType.DEVICE, device.getUuidId().toString());
-					saveAttributes(tenantId, entityId, "SERVER_SCOPE", attributes);
 
-					List<AttributeKvEntry> attributesShare = new ArrayList<>();
-					String tShare = mapper.writeValueAsString(deviceInfo.getDeviceShareAttrib());
-					Map mShare = mapper.readValue(tShare, Map.class);
-					mShare.forEach((key, value) -> {
-						if (value != null) {
-							attributesShare.add(new BaseAttributeKvEntry(new StringDataEntry(key.toString(), value.toString()), System.currentTimeMillis()));
-						}
-					});
-					saveAttributes(tenantId, entityId, "SHARED_SCOPE", attributesShare);
+					if (deviceInfo.getDeviceServerAttrib() != null) {
+						String t = mapper.writeValueAsString(deviceInfo.getDeviceServerAttrib());
+						Map m = mapper.readValue(t, Map.class);
+						List<AttributeKvEntry> attributes = new ArrayList<>();
+						m.forEach((key, value) -> {
+							if (value != null) {
+								attributes.add(new BaseAttributeKvEntry(new StringDataEntry(key.toString(), value.toString()), System.currentTimeMillis()));
+							}
+						});
 
-
-					List<AttributeKvEntry> attributesClient = new ArrayList<>();
-					String tClient = mapper.writeValueAsString(deviceInfo.getDeviceClientAttrib());
-					Map mClient = mapper.readValue(tClient, Map.class);
-					mClient.forEach((key, value) -> {
-						if (value != null) {
-							attributesClient.add(new BaseAttributeKvEntry(new StringDataEntry(key.toString(), value.toString()), System.currentTimeMillis()));
-						}
-					});
-					saveAttributes(tenantId, entityId, "CLIENT_SCOPE", attributesClient);
-
-
+						saveAttributes(a.getTenantId(), entityId, "SERVER_SCOPE", attributes);
+					}
+					else {
+						throw new ThingsboardException(ThingsboardErrorCode.BAD_REQUEST_PARAMS);
+					}
+					if (deviceInfo.getDeviceShareAttrib() != null) {
+						List<AttributeKvEntry> attributesShare = new ArrayList<>();
+						String tShare = mapper.writeValueAsString(deviceInfo.getDeviceShareAttrib());
+						Map mShare = mapper.readValue(tShare, Map.class);
+						mShare.forEach((key, value) -> {
+							if (value != null) {
+								attributesShare.add(new BaseAttributeKvEntry(new StringDataEntry(key.toString(), value.toString()), System.currentTimeMillis()));
+							}
+						});
+						saveAttributes(a.getTenantId(), entityId, "SHARED_SCOPE", attributesShare);
+					}
+					else {
+						throw new ThingsboardException(ThingsboardErrorCode.BAD_REQUEST_PARAMS);
+					}
+					if (deviceInfo.getDeviceClientAttrib() != null) {
+						List<AttributeKvEntry> attributesClient = new ArrayList<>();
+						String tClient = mapper.writeValueAsString(deviceInfo.getDeviceClientAttrib());
+						Map mClient = mapper.readValue(tClient, Map.class);
+						mClient.forEach((key, value) -> {
+							if (value != null) {
+								attributesClient.add(new BaseAttributeKvEntry(new StringDataEntry(key.toString(), value.toString()), System.currentTimeMillis()));
+							}
+						});
+						saveAttributes(a.getTenantId(), entityId, "CLIENT_SCOPE", attributesClient);
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -243,15 +189,13 @@ public class BatchConfigController extends BaseController {
 		} catch (Exception e) {
 			handleException(e);
 		}
-
 		return null;
 	}
 
 	private void saveAttributes(TenantId srcTenantId,
-														  EntityId entityIdSrc,
-														  String scope,
-														  List<AttributeKvEntry> attributes) throws ThingsboardException {
-
+								EntityId entityIdSrc,
+								String scope,
+								List<AttributeKvEntry> attributes) throws ThingsboardException {
 		SecurityUser user = getCurrentUser();
 
 		tsSubService.saveAndNotify(srcTenantId, entityIdSrc, scope, attributes, new FutureCallback<Void>() {
@@ -272,7 +216,7 @@ public class BatchConfigController extends BaseController {
 			}
 		});
 
-		return  ;
+		return;
 
 	}
 
@@ -283,5 +227,66 @@ public class BatchConfigController extends BaseController {
 		} catch (ThingsboardException te) {
 			log.warn("Failed to log attributes update", te);
 		}
+	}
+
+	/** 
+	* @Description: 获取设备属性并生成列表
+	* @Author: ShenJi
+	* @Date: 2019/2/25 
+	* @Param: [tenantId, relations] 
+	* @return: java.util.List<org.thingsboard.server.common.data.batchconfig.DeviceAutoLogon>
+	*/ 
+	private List<DeviceAutoLogon> getDevicesAttrib(TenantId tenantId, List<EntityRelation> relations) {
+		List<DeviceAutoLogon> ret = new ArrayList<>();
+
+		relations.stream()
+				.filter(p -> {
+					return p.getTo().getEntityType() == EntityType.DEVICE;
+				})
+				.forEach((relation) -> {
+					DeviceAutoLogon deviceAutoLogon = new DeviceAutoLogon();
+					try {
+						Map<String, Object> clientMap = new HashMap<>();
+						List<AttributeKvEntry> attributeKvEntries = attributesService.findAll(tenantId, relation.getTo(), "CLIENT_SCOPE").get();
+						attributeKvEntries.forEach((attributeKvEntry -> {
+							clientMap.put(attributeKvEntry.getKey(), attributeKvEntry.getValue().toString());
+						}));
+						deviceAutoLogon.setDeviceClientAttrib(MAPPER.readValue(MAPPER.writeValueAsString(clientMap), DeviceClientAttrib.class));
+
+						attributeKvEntries = attributesService.findAll(tenantId, relation.getTo(), "SERVER_SCOPE").get();
+						Map<String, Object> serverMap = new HashMap<>();
+						attributeKvEntries.forEach((attributeKvEntry -> {
+							serverMap.put(attributeKvEntry.getKey(), attributeKvEntry.getValue().toString());
+						}));
+						deviceAutoLogon.setDeviceServerAttrib(MAPPER.readValue(MAPPER.writeValueAsString(serverMap), DeviceServerAttrib.class));
+
+						attributeKvEntries = attributesService.findAll(tenantId, relation.getTo(), "SHARED_SCOPE").get();
+						Map<String, Object> shareMap = new HashMap<>();
+						attributeKvEntries.forEach((attributeKvEntry -> {
+							shareMap.put(attributeKvEntry.getKey(), attributeKvEntry.getValue().toString());
+						}));
+						deviceAutoLogon.setDeviceShareAttrib(MAPPER.readValue(MAPPER.writeValueAsString(shareMap), DeviceShareAttrib.class));
+						if ( null == deviceAutoLogon.getDeviceShareAttrib().getToken()){
+							DeviceShareAttrib tmp = deviceAutoLogon.getDeviceShareAttrib();
+							tmp.setToken(new String(deviceCredentialsService.findDeviceCredentialsByDeviceId(null,new DeviceId(relation.getTo().getId())).getCredentialsId()));
+							deviceAutoLogon.setDeviceShareAttrib(tmp);
+						}
+						if ( deviceAutoLogon.getDeviceShareAttrib().getToken().isEmpty()){
+							deviceAutoLogon.getDeviceShareAttrib().setToken(deviceCredentialsService.findDeviceCredentialsByDeviceId(null,new DeviceId(relation.getTo().getId())).getCredentialsId());
+						}
+
+						deviceAutoLogon.setSystemDeviceId(relation.getTo().getId().toString());
+
+						ret.add(deviceAutoLogon);
+
+					} catch (Exception e) {
+						log.info("Get device client attrib error:" + relation.getTo().getId());
+						e.printStackTrace();
+						handleException(e);
+					}
+
+				});
+
+		return ret;
 	}
 }
