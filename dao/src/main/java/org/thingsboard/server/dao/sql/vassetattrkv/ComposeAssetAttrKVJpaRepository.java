734e092cb8760a09f6ed07b7d698dd55f9c42b6f
shjmqqc <shjmqqc@189.cn>
2019-03-06 22:12:32 +0800

    1.2.7.13 按组合键查询资产属性

--- dao/src/main/java/org/thingsboard/server/dao/sql/vassetattrkv/ComposeAssetAttrKVJpaRepository.java
+++ dao/src/main/java/org/thingsboard/server/dao/sql/vassetattrkv/ComposeAssetAttrKVJpaRepository.java
@@ -38,4 +38,13 @@ public interface ComposeAssetAttrKVJpaRepository extends CrudRepository<ComposeA
 			"WHERE a.entity_id in (SELECT id FROM public.asset WHERE tenant_id = :tenantId)"
 			,nativeQuery = true)
 	List<ComposeAssetAttrKV> findByTenantIdAndComposekey(@Param("tenantId") String tenantId, @Param("attr_key1")String attr_key1, @Param("attr_key2")String attr_key2);//1e918a07edf05d0893415428c622f6e
+
+	@Query(value = "SELECT a.entity_type, a.entity_id, a.attribute_type, a.attribute_key as attr_key1,a.str_v as strv1,b.attribute_key as attr_key2, b.str_v as strv2, a.last_update_ts " +
+			"FROM (SELECT * FROM public.attribute_kv WHERE attribute_key = :attr_key1) a " +
+			"join " +
+			"(SELECT * FROM public.attribute_kv WHERE attribute_key = :attr_key2) b  " +
+			"on a.entity_id = b.entity_id  " +
+			"WHERE a.entity_id in (SELECT id FROM public.asset WHERE customer_id = :customerId)"
+			,nativeQuery = true)
+	List<ComposeAssetAttrKV> findByCustomerIdAndComposekey(@Param("customerId") String customerId, @Param("attr_key1")String attr_key1, @Param("attr_key2")String attr_key2);//1e918a07edf05d0893415428c622f6e
 }
