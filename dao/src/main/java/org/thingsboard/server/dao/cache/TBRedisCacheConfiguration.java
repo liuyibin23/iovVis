7132f6e7ac391a789adf6837823c9d0ffa386230
liuyibin <liuyibin23@163.com>
2019-06-19 16:04:44 +0800

    redis集群配置

--- dao/src/main/java/org/thingsboard/server/dao/cache/TBRedisCacheConfiguration.java
+++ dao/src/main/java/org/thingsboard/server/dao/cache/TBRedisCacheConfiguration.java
@@ -24,9 +24,14 @@ import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisNode;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @ConditionalOnProperty(prefix = "cache", value = "type", havingValue = "redis", matchIfMissing = false)
@@ -46,11 +51,45 @@ public class TBRedisCacheConfiguration {
     @Value("${redis.connection.password}")
     private String password;
 
+    @Value("${redis.connection.clusterNode}")
+    private String clusterNode;
+
     @Bean
+    @ConditionalOnProperty(prefix = "redis.connection", value = "type", havingValue = "standalone", matchIfMissing = false)
     public RedisConnectionFactory redisConnectionFactory() {
         JedisConnectionFactory factory = new JedisConnectionFactory();
         factory.setHostName(host);
         factory.setPort(port);
+        factory.setDatabase(db);
+        factory.setPassword(password);
+        return factory;
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "redis.connection", value = "type", havingValue = "cluster", matchIfMissing = false)
+    public RedisClusterConfiguration getRedisCluster() {
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration();
+        String [] serverArray=clusterNode.split(",");
+        Set<RedisNode> jedisClusterNodes = new HashSet<RedisNode>();
+        for (String ipPort:serverArray){
+            String [] ipPortPair=ipPort.split(":");
+            jedisClusterNodes.add(new RedisNode(ipPortPair[0],Integer.valueOf(ipPortPair[1].trim())));
+        }
+//        jedisClusterNodes.add(new RedisNode("192.168.1.142", 7000));
+//        jedisClusterNodes.add(new RedisNode("192.168.1.142", 7001));
+//        jedisClusterNodes.add(new RedisNode("192.168.1.142", 7002));
+//        jedisClusterNodes.add(new RedisNode("192.168.1.142", 7003));
+//        jedisClusterNodes.add(new RedisNode("192.168.1.142", 7004));
+//        jedisClusterNodes.add(new RedisNode("192.168.1.142", 7005));
+        redisClusterConfiguration.setClusterNodes(jedisClusterNodes);
+        return redisClusterConfiguration;
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "redis.connection", value = "type", havingValue = "cluster", matchIfMissing = false)
+    public RedisConnectionFactory redisClusterConnectionFactory(RedisClusterConfiguration redisClusterConfiguration){
+        JedisConnectionFactory factory = new JedisConnectionFactory(redisClusterConfiguration);
+//        redisConnectionFactory.setPoolConfig(jedisPoolConfig);
         factory.setDatabase(db);
         factory.setPassword(password);
         return factory;
